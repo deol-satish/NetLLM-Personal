9ec011f6a8ec1b94e249a2286c720a70ef341d17
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-07-30 18:33:21 +1000

    Original
    Initial commit
    
    First Commit - Exact Copy of NetLLM
    
    Made Few changes - for debugging
    
    Create urllc.yml
    
    output some exp pool data
    
    Create pod_inst.sh
    
    Update pod_inst.sh
    
    Update pod_inst.sh

---
+++ ins_run_pod.sh
@@ -0,0 +1,8 @@
+python -m pip install --upgrade pip
+pip intsall munch
+pip install peft==0.6.2
+pip install yacs
+pip install openprompt==1.0.1
+pip install transformers==4.34.1
+pip install --upgrade huggingface_hub accelerate
+pip install scikit-learn
---
+++ pod_inst.sh
@@ -0,0 +1,29 @@
+ssh root@69.30.85.20 -p 22135 -i ~/.ssh/id_ed25519
+
+
+ssh root@69.30.85.20 -p 22057 -i ~/.ssh/id_ed25519
+
+chmod 600 ~/.ssh/id_ed25519;chmod 600 ~/.ssh/id_ed25519.pub
+
+
+sudo scp -P 3322 -p -i ~/.ssh/mptcprootkey -r NetLLM-Personal root@192.168.56.1:
+sudo scp -334423 -p -i ~/.ssh/mptcprootkey -r NetLtLLM-Personal root@192.168.56.1:
+
+scp -P 22135 -i ~/.ssh/id_ed25519 -r NetLLM-Personal root@69.30.85.20:/path/to/destination/
+
+ssh root@69.30.85.48 -p 22177 -i ~/.ssh/id_ed25519
+
+
+sudo scp -P 22177 -p -i ~/.ssh/id_ed25519 -r NetLLM-Personal root@69.30.85.48:
+
+ssh root@69.30.85.24 -p 22163 -i ~/.ssh/id_ed25519
+
+
+
+python run_plm.py --adapt --grad-accum-steps 32 --plm-type llama --plm-size base --rank 128 --device cuda:0 --device-out cuda:1 --lr 0.0001 --warmup-steps 2000 --num-epochs 80 --eval-per-epoch 2 
+python run_plm.py --adapt --grad-accum-steps 32 --plm-type llama --plm-size base --rank 128 --device cuda:0 --device-out cuda:1 --lr 0.0001 --warmup-steps 1000 --num-epochs 20 --eval-per-epoch 2
+
+
+
+Test performance of NetLLM:
+python run_plm.py --test --grad-accum-steps 32 --plm-type llama --plm-size base --rank 128 --device cuda:0 --device-out cuda:1 --lr 0.0001 --warmup-steps 2000 --num-epochs 80 --eval-per-epoch 2
